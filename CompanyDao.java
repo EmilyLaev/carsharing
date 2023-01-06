6da8d94fecead346845e59c5da5fdd400885c726
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-05 21:21:49 -0500

    Update CompanyDao.java

--- CompanyDao.java
+++ CompanyDao.java
@@ -2,7 +2,6 @@ package carsharing;
 
 import java.util.List;
 
-
 //This is the interface for a data access object (DAO) so that we can use database operations on the Company
 public interface CompanyDao {
     List<Company> getAllCompanies();
