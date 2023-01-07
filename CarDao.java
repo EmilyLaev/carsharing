c67b458677d8f003902f7c7b61bc246ccbffc5af
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 15:06:37 -0500

    Update CarDao.java

--- CarDao.java
+++ CarDao.java
@@ -2,6 +2,8 @@ package carsharing;
 
 import java.util.List;
 
+//Interface for interacting with a database that stores information about cars in a car-sharing service.
+//Returns a list of Car objects that belong to a specific company, identified by the companyId argument.
 public interface CarDao {
     List<Car> getCars(int companyId);
 
