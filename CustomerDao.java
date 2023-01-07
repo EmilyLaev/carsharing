31292472eef173db86b8d25ef558efb0203ed8f2
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 16:05:26 -0500

    Update CustomerDao.java

--- CustomerDao.java
+++ CustomerDao.java
@@ -2,6 +2,10 @@ package carsharing;
 
 import java.util.List;
 
+// Defines methods for interacting with a database of customers of a car-sharing service.
+//Returns a list of all the customers in the database.
+//Creates a new customer with the given name and adds it to the database
+//Marks the car with the given identifier as being available for rent and unlinks it from thecustomer with the given identifier.
 public interface CustomerDao {
     List<Customer> getAllCustomers();
 
