46eef384807200a46fc9cbf23f0e502fd9dc2854
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 16:02:19 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,11 +1,14 @@
 package carsharing;
 
+//Represents a customer of a car-sharing service.
 public class Customer {
     private int id;
     private String name;
     private int carId;
 
 
+    //Constructor that takes three arguments: the customer's identifier, name, and car identifier,
+    //and initializes the id, name, and carId fields with these values.
     public Customer(int id, String name, int carId) {
         this.id = id;
         this.name = name;
@@ -13,6 +16,9 @@ public class Customer {
 
     }
 
+    //Constructor that takes two arguments: the customer's identifier and name, and initializes the
+    //id and name fields with these values. It also sets the carId field to 0, which indicates that
+    //the customer is not currently using any car.
     public Customer(int id, String name) {
         this.id = id;
         this.name = name;
@@ -20,6 +26,7 @@ public class Customer {
 
     }
 
+    //The getters and setters for the customer
     public int getId() {
         return id;
     }
