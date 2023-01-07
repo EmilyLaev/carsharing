907ec5e31e939ea49e0a9e5dd131a3d332577f3d
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 16:23:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,6 +19,10 @@ import java.sql.Statement;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+//Main class for a car rental application
+// Gets the name of the database file to use
+// Creates a ControllerH2 object using the database name
+// Creates a CompanyDaoH2Impl object using the connection from the ControllerH2 object
 public class Main {
 
     public static void main(String[] args) {
@@ -28,18 +32,23 @@ public class Main {
         CarDaoH2Impl carDaoH2 = new CarDaoH2Impl(controllerH2.getConnection());
         CustomerDaoH2Impl customerDaoH2 = new CustomerDaoH2Impl(controllerH2.getConnection());
 
+        //Enter an infinite loop to display the main menu and perform actions based on user input
         for(;;) {
             MenuPrinter.mainMenu();
             switch (Menu.userChoice()) {
+                // Management of companies and cars
                 case 1:
                     Menu.managementCompany(companyDaoH2, carDaoH2);
                     break;
+                // Management of customers and rented cars
                 case 2:
                     Menu.managementCustomerMenu(customerDaoH2, companyDaoH2, carDaoH2);
                     break;
+                // Creation of new customers
                 case 3:
                     Menu.managementCreateCustomer(customerDaoH2);
                     break;
+                // Exit the program
                 case 0:
                     controllerH2.closeConnection();
                     return;
@@ -47,7 +56,7 @@ public class Main {
         }
     }
 
-
+    // Helper method to get the name of the database file to use.
     private static String getDbName(String[] args) {
         if (args.length >= 2 && args[0].equals("-databaseFileName")) {
             return args[1];
