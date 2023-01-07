4a1f57e88e6fe6ceeaf9345e54c777646d4b2246
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 16:45:22 -0500

    Update MenuPrinter.java

--- MenuPrinter.java
+++ MenuPrinter.java
@@ -1,7 +1,9 @@
 package carsharing;
 
+//This is a class used to print the various menus
 public class MenuPrinter {
 
+    //A method to print the main menu
     public static void mainMenu() {
         System.out.println("1. Log in as a manager");
         System.out.println("2. Log in as a customer");
@@ -9,18 +11,21 @@ public class MenuPrinter {
         System.out.println("0. Exit");
     }
 
+    //A method to print the manager menu
     public static void managerMenu() {
         System.out.println("1. Company list");
         System.out.println("2. Create a company");
         System.out.println("0. Back");
     }
 
+    //A method to print the company menu
     public static void companyMenu() {
         System.out.println("1. Car list");
         System.out.println("2. Create a car");
         System.out.println("0. Back");
     }
 
+    //A method to print the customer menu
     public static void customerMenu() {
         System.out.println("1. Rent a car");
         System.out.println("2. Return a rented car");
