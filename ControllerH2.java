153d3f9a40543a37a2cbb218c88731d5f891b24b
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 15:58:51 -0500

    Update ControllerH2.java

--- ControllerH2.java
+++ ControllerH2.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//Controller class for managing a database connection for a car-sharing service.
 public class ControllerH2 {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "org.h2.Driver";
@@ -11,13 +12,15 @@ public class ControllerH2 {
 
     private Connection connection;
 
+    //Creates a connection to the database using the JDBC_DRIVER and DB_URL constants
+    //and the specified database name.
     private static Connection createConnection(String dbName) throws SQLException, ClassNotFoundException {
         Class.forName(JDBC_DRIVER);
         return DriverManager.getConnection(DB_URL + dbName);
     }
 
-    ;
-
+    //Constructor that takes a database name and creates a connection to the database using the
+    //createConnection() method. The connection's auto-commit mode is set to true.
     public ControllerH2(String dbName) {
         try {
             connection = createConnection(dbName);
@@ -31,10 +34,12 @@ public class ControllerH2 {
         }
     }
 
+    //Returns the connection to the database.
     public Connection getConnection() {
         return connection;
     }
 
+    //Closes the connection to the database if it is not null
     public void closeConnection() {
         try {
             if (connection != null) connection.close();
@@ -42,5 +47,4 @@ public class ControllerH2 {
             se.printStackTrace();
         }
     }
-
 }
